ce4ac76bc9830fd480b99a545cdcea148fb247c6
Zachary Frederick <zachfred62@gmail.com>
2019-03-28 21:29:49 -0400

    poop

--- FuseManager.cpp
+++ FuseManager.cpp
@@ -5,23 +5,3 @@
  *      Author: zacharyfrederick
  */
 
-#include "FuseManager.h"
-
-FuseManager::FuseManager() {
-	// TODO Auto-generated constructor stub
-
-}
-
-FuseManager::~FuseManager() {
-	// TODO Auto-generated destructor stub
-}
-
-bool FuseManager::copy_data(uint8_t* raw_data, int size) {
-	this->data = new uint8_t[size];
-
-	for(int i=0; i < size; i++) {
-		//FuseManager::data[i] = raw_data[i];
-	}
-
-	return true;
-}
--- FuseManager.h
+++ FuseManager.h
@@ -8,16 +8,5 @@
 #ifndef FUSEMANAGER_H_
 #define FUSEMANAGER_H_
 
-#include <cstdint>
-
-using namespace std;
-class FuseManager {
-public:
-	FuseManager();
-	virtual ~FuseManager();
-	static uint8_t *data;
-	bool copy_data(uint8_t *raw_data, int size);
-
-};
 
 #endif /* FUSEMANAGER_H_ */
